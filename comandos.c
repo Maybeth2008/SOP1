1740deee9c9ff1bc2f76dc523bbd2b27df44562a
Maybeth2008 <lopezbedoya.2008@gmail.com>
2026-08-31 23:53:04 -0500

    Add file creation and user input handling
    Implement function to create a file and write user input to it.

---
+++ comandos.c
@@ -0,0 +1,40 @@
+#include "lista.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+int crearArchivo(char *nombreArchivo) {
+
+    int fd=open(nombreArchivo,  O_CREAT | O_RDWR , 0644);
+
+    if(fd==-1){
+        printf("Error al abrir el archivo");
+        return 1;
+    }
+    
+    int size=100000;
+    char *palabra=malloc(size);
+    int terminador=0;
+    char buffer[100000]="";
+
+    while(true){
+        // %s guarda cadena de caracteres, hasta encontrar un espacio o salto de línea
+        palabra=fgets(palabra, size, stdin);
+        //leer un numero por consola
+        scanf("%d", &terminador); // %d guarda un entero
+        //suma los caracteres de palabra y los acumula en buffer
+        strcat(buffer, palabra);
+        getchar(); // Limpiar el enter que queda después de leer el número
+        if(terminador==2){
+            break;
+        }
+    }
+    //lee el texto acumulado en buffer y lo escribe en el archivo
+    write(fd, buffer, strlen(buffer));
+    
+    // Cerrar el archivo
+    close(fd);
+    free(palabra);
+    return 0;
+}
